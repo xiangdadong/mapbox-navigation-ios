d8b3a84a1605efd928747763f1c8d52466ffd3dd
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-09-03 20:15:19 +0200

    Added u-turn test and fixtures

--- MapboxNavigationTests/ManeuverViewTests.swift
+++ MapboxNavigationTests/ManeuverViewTests.swift
@@ -14,40 +14,59 @@ class ManeuverViewTests: FBSnapshotTestCase {
         maneuverView.backgroundColor = .white
         recordMode = false
         isDeviceAgnostic = true
-        usesDrawViewHierarchyInRect = true
         
         let window = UIWindow(frame: maneuverView.bounds)
         window.addSubview(maneuverView)
     }
     
-    func maneuverInstruction(_ maneuverType: ManeuverType, _ maneuverDirection: ManeuverDirection, _ drivingSide: DrivingSide, _ degrees: CLLocationDegrees = 180) -> VisualInstruction {
+    func maneuverInstruction(_ maneuverType: ManeuverType, _ maneuverDirection: ManeuverDirection, _ degrees: CLLocationDegrees = 180) -> VisualInstruction {
         let component = VisualInstructionComponent(type: .delimiter, text: "", imageURL: nil, abbreviation: nil, abbreviationPriority: 0)
         return VisualInstruction(text: "", maneuverType: maneuverType, maneuverDirection: maneuverDirection, components: [component], degrees: degrees)
     }
     
     func testStraightRoundabout() {
-        maneuverView.visualInstruction = maneuverInstruction(.takeRoundabout, .straightAhead, .right)
+        maneuverView.visualInstruction = maneuverInstruction(.takeRoundabout, .straightAhead)
         FBSnapshotVerifyLayer(maneuverView.layer)
     }
     
     func testTurnRight() {
-        maneuverView.visualInstruction = maneuverInstruction(.turn, .right, .right)
+        maneuverView.visualInstruction = maneuverInstruction(.turn, .right)
         FBSnapshotVerifyLayer(maneuverView.layer)
     }
     
     func testTurnSlightRight() {
-        maneuverView.visualInstruction = maneuverInstruction(.turn, .slightRight, .right)
+        maneuverView.visualInstruction = maneuverInstruction(.turn, .slightRight)
         FBSnapshotVerifyLayer(maneuverView.layer)
     }
     
     func testMergeRight() {
-        maneuverView.visualInstruction = maneuverInstruction(.merge, .right, .right)
+        maneuverView.visualInstruction = maneuverInstruction(.merge, .right)
         FBSnapshotVerifyLayer(maneuverView.layer)
     }
     
     func testRoundaboutTurnLeft() {
-        maneuverView.visualInstruction = maneuverInstruction(.takeRoundabout, .right, .right, CLLocationDegrees(270))
+        maneuverView.visualInstruction = maneuverInstruction(.takeRoundabout, .right, CLLocationDegrees(270))
         FBSnapshotVerifyLayer(maneuverView.layer)
+    }
+    
+    func testLeftUTurn() {
+        maneuverView.drivingSide = .right
+        maneuverView.visualInstruction = maneuverInstruction(.turn, .uTurn)
+        // Layer transformations are not rendered when snapshotting so we
+        // manually flip the U-turn for right-hand rule of the road in this test.
+        let image = maneuverView.asImage()
+        let flipped = UIImage(cgImage: image.cgImage!, scale: UIScreen.main.scale, orientation: .upMirrored)
+        let imageView = UIImageView(image: flipped)
+        FBSnapshotVerifyView(imageView)
+    }
+    
+    func testRightUTurn() {
+        maneuverView.drivingSide = .left
+        maneuverView.visualInstruction = maneuverInstruction(.turn, .uTurn)
+        FBSnapshotVerifyLayer(maneuverView.layer)
+        let image = maneuverView.asImage()
+        let imageView = UIImageView(image: image)
+        FBSnapshotVerifyView(imageView)
     }
     
     func testRoundabout() {
@@ -59,7 +78,7 @@ class ManeuverViewTests: FBSnapshotTestCase {
             let position = CGPoint(x: maneuverView.bounds.width * (bearing / incrementer), y: 0)
             let view = ManeuverView(frame: CGRect(origin: position, size: maneuverView.bounds.size))
             view.backgroundColor = .white
-            view.visualInstruction = maneuverInstruction(.takeRoundabout, .right, .right, CLLocationDegrees(bearing))
+            view.visualInstruction = maneuverInstruction(.takeRoundabout, .right, CLLocationDegrees(bearing))
             views.addSubview(view)
         }
         
@@ -69,3 +88,13 @@ class ManeuverViewTests: FBSnapshotTestCase {
     // TODO: Figure out why the flip transformation do not render in a snapshot so we can test left turns and left side rule of the road.
 }
 
+extension UIView {
+    
+    func asImage() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, true, UIScreen.main.scale)
+        drawHierarchy(in: bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
